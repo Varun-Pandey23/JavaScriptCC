df2641bfd199736b0510b927bfc7c30665f9e834
Varun-Pandey23 <154004632+Varun-Pandey23@users.noreply.github.com>
2025-03-21 23:04:42 +0530

    Update test3.js

--- basics/test3.js
+++ basics/test3.js
@@ -3,3 +3,5 @@ console.log(typeof(num));
 let num1=Number(num); //typecasting
 console.log(typeof num1);
 console.log(num1);
+//stack memory - for primitive data types
+//heap memory - for non-primitive data types
