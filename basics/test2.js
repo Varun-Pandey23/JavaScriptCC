c712736f23d1be0a5ba758239ad77cf67a81aa8a
Varun-Pandey23 <154004632+Varun-Pandey23@users.noreply.github.com>
2025-03-21 20:03:10 +0530

    Update test2.js

--- basics/test2.js
+++ basics/test2.js
@@ -1,3 +1,5 @@
 "use strict" //treat all js code as newer version
 let name="xyz";
 var notuse; //var can be accessed and modified outside scope therefore not use
+console.log(typeof name) //string
+console.log(typeof null) //object
